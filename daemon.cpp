a887f3efacc86d56f0a6515d4c1474fb3c7292ec
WalterJ726 <walterjiang726@gmail.com>
2023-04-01 11:16:02 -0400

    first commit

---
+++ daemon.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+
+int main()
+{
+    // if(daemon(1, 0) == -1){
+    //     std::cout<<"error\n"<<std::endl;
+    //     exit(-1);
+    // }
+    return EXIT_SUCCESS;
+}
