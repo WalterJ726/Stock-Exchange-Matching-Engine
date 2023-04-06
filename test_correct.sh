6fc9a0e6e9b00239f5a0374af33b4d18e6976edf
Jingzhi Zhao <jingzhi.zhao@duke.edu>
2023-04-06 03:50:26 +0200

    Add testcases

--- test_correct.sh
+++ test_correct.sh
@@ -1,22 +1,5 @@
 HOSTNAME=localhost
 
-<<<<<<< HEAD
-#./testHandleRequest $HOSTNAME 12345 testcase/add_acount.xml
-#./testHandleRequest $HOSTNAME 12345 testcase/transactions1.xml
-#./testHandleRequest $HOSTNAME 12345 testcase/transactions2.xml
-#./testHandleRequest $HOSTNAME 12345 testcase/transactions3.xml
-#./testHandleRequest $HOSTNAME 12345 testcase/transactions4.xml
-#./testHandleRequest $HOSTNAME 12345 testcase/transactions5.xml
-#./testHandleRequest $HOSTNAME 12345 testcase/transactions6.xml
-#./testHandleRequest $HOSTNAME 12345 testcase/transactions7.xml
-#./testHandleRequest $HOSTNAME 12345 testcase/transactions_query1.xml
-./testHandleRequest $HOSTNAME 12345 testcase/my_add_account.xml 
-./testHandleRequest $HOSTNAME 12345 testcase/my_trans_1.xml 
-./testHandleRequest $HOSTNAME 12345 testcase/my_trans_2.xml 
-./testHandleRequest $HOSTNAME 12345 testcase/my_trans_3.xml 
-./testHandleRequest $HOSTNAME 12345 testcase/my_trans_4.xml 
-./testHandleRequest $HOSTNAME 12345 testcase/my_trans_5.xml 
-=======
 ./testHandleRequest $HOSTNAME 12345 testcase/add_acount.xml
 ./testHandleRequest $HOSTNAME 12345 testcase/transactions1.xml
 ./testHandleRequest $HOSTNAME 12345 testcase/transactions2.xml
@@ -26,4 +9,15 @@ HOSTNAME=localhost
 ./testHandleRequest $HOSTNAME 12345 testcase/transactions6.xml
 ./testHandleRequest $HOSTNAME 12345 testcase/transactions7.xml
 ./testHandleRequest $HOSTNAME 12345 testcase/transactions_query1.xml
->>>>>>> c0cb50ae3107f711fd9fb662e926b1ffc520208f
+#./testHandleRequest $HOSTNAME 12345 testcase/my_add_account.xml 
+#./testHandleRequest $HOSTNAME 12345 testcase/my_trans_1.xml 
+#./testHandleRequest $HOSTNAME 12345 testcase/my_trans_2.xml 
+#./testHandleRequest $HOSTNAME 12345 testcase/my_trans_3.xml 
+#./testHandleRequest $HOSTNAME 12345 testcase/my_trans_4.xml 
+#./testHandleRequest $HOSTNAME 12345 testcase/my_trans_5.xml 
+#./testHandleRequest $HOSTNAME 12345 testcase/error1//my_add_account.xml
+#./testHandleRequest $HOSTNAME 12345 testcase/error1/my_trans_1.xml 
+#./testHandleRequest $HOSTNAME 12345 testcase/error1/my_trans_2.xml 
+#./testHandleRequest $HOSTNAME 12345 testcase/error1/my_trans_3.xml 
+#./testHandleRequest $HOSTNAME 12345 testcase/error1/my_trans_4.xml 
+
